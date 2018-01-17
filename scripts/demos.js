3e22b0bd6291cca4677f344f34d55a06a6a65507
Mark Lennox <mark.lennox@hmhco.com>
2018-01-17 17:49:53 +0000

    adding localStorage

--- scripts/demos.js
+++ scripts/demos.js
@@ -1,21 +1,27 @@
 class Demos {
+  constructor() {
+    this.someDoc = this.generateDoc(1000);
+  }
+
   /**
    * attaches listener to a container to catch bubbled clicks
    * we will check if the click comes from a button
    * @param {*} containerId
    */
-  onBubbledClick(containerId) {
-    document.getElementById(containerId).addEventListener(
-      "click",
-      event => {
-        if (event.target.tagName === "BUTTON") {
-          let size = event.target.dataset.size;
-          let handlerName = event.target.dataset.handler;
-          this[handlerName](size);
-        }
-      },
-      false
-    );
+  onBubbledClick(containerIds) {
+    containerIds.forEach(id => {
+      document.getElementById(id).addEventListener(
+        "click",
+        event => {
+          if (event.target.tagName === "BUTTON") {
+            let size = event.target.dataset.size;
+            let handlerName = event.target.dataset.handler;
+            this[handlerName](size);
+          }
+        },
+        false
+      );
+    });
   }
 
   bigCalc(size) {
@@ -24,14 +30,34 @@ class Demos {
     for (let i = 0; i < size; i++) {
       Math.random();
     }
-    console.log(`for ${size} it took ${Date.now() - start}ms`);
+    console.log(`bigCalc : for ${size} it took ${Date.now() - start}ms`);
+  }
+
+  generateDoc(limit) {
+    let doc = {};
+    for (let i = 0; i < limit; i++) {
+      doc[`node-${i}`] = {
+        namer: `name ${i}`,
+        somedata: Math.random()
+      };
+    }
+    return JSON.stringify(doc);
+  }
+
+  storeIt(size) {
+    const start = Date.now();
+    size = size || 1000;
+    for (let i = 0; i < size; i++) {
+      localStorage.setItem(`${size}`, this.someDoc);
+    }
+    console.log(`storeIt : for ${size} it took ${Date.now() - start}ms`);
   }
 }
 
 const setupDemos = () => {
   //   add listeners
   const demos = new Demos();
-  demos.onBubbledClick("vanillacpu");
+  demos.onBubbledClick(["vanillacpu", "vanillastorage"]);
 };
 
 if (
